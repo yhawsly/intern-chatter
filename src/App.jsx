68e2b9e5efe2729b6f57896d6d79cd1d8608cd69
Sam <atsyatsya1@gmail.com>
2023-12-01 03:06:44 +0000

    Permission Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './globals.css'
+import Modal from './Components/Modal.jsx';
 function App() {
+  const [publishModel,setOpenModel] = useState(false); 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <h1 className="text-2xl font-semibold text-gray-600 text-center">
-        The Intern Chatter Project
+        <p>The Intern Chatter Project</p>
+        <button className=' w-24 h-12 font-bold bg-blue-500 text-zinc-200 rounded-xl  ' 
+      onClick={() => {
+        setOpenModel(true);
+      }}>
+        Publish</button>
+      {publishModel && <Modal closeModel ={setOpenModel} />}
       </h1>
     </div>
   );
