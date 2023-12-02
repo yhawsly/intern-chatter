923adbb20f0782f7bef4e12382a106c2170b8989
Hittloson <hittlo86@gmail.com>
2023-12-02 23:24:14 +0000

    Like and Comment Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <h1 className="text-2xl font-semibold text-gray-600 text-center">
         The Intern Chatter Project
       </h1>
-      
     </div>
   );
 }
